c738aab0727b809113c2ada05dbaeb7c1826c2fe
polhek <ziga.groselj@gmail.com>
2020-08-04 17:04:02 +0200

    Added all the javascript functionality, now everything works as intended... Will add localstorage/firebase

--- index.js
+++ index.js
@@ -63,39 +63,64 @@ document.querySelector(".book-form").addEventListener("submit", (e) => {
     const book = new Book(title, author, pages, readStatus);
     myLibrary.push(book);
     addNewBookUI(book);
-    clearFormFields()
+    clearFormFields();
   }
 });
 
 function addNewBookUI(book) {
   if (book.readStatus === "yes") {
-    i = "checked";
+    i = "Read";
   } else {
-    i = "";
+    i = "Not read";
   }
   const main = document.querySelector(".main");
   const bookCard = document.createElement("div");
   bookCard.classList.add("book-card");
   bookCard.innerHTML = `<div class="delete_button"><button class="delete btn"><i class="fa fa-trash">
         </i></button></div><div class="title">${book.title}</div><div class="author">${book.author}
-        </div><div class="pages">${book.pages}</div><div class="read_status">Read: <input type="checkbox" id="yes" name="readstatus" value="yes" ${i}> 
-        </div>`;
+        </div><div class="pages">${book.pages}</div><div class="button_status"><button class="book_status">${i}</button></div>`;
   main.appendChild(bookCard);
 }
 
-// clear form fields after submit 
+// clear form fields after submit
 function clearFormFields() {
   const myForm = document.getElementById("myForm");
   myForm.reset();
 }
 
-// Add event listener to all deleteButton 
-const deleteButton = document.querySelectorAll(".delete");
+// Add event listener to all current and future deleteButtons
+document.querySelector(".main").onclick = (ev) => {
+  let el = ev.target.classList.contains("fa-trash")
+    ? ev.target.parentElement
+    : ev.target.classList.contains("delete")
+    ? ev.target
+    : false;
+  if (el) {
+    let card = el.parentElement.parentElement;
+    // remove array-element from myLibrary...
+    myLibrary.splice([...card.parentElement.children].indexOf(card), 1);
+    //remove DOM of book...
+    card.remove();
+  }
+};
 
-// deletes book UI;
-deleteButton.forEach((el) => {
-    el.addEventListener("click", function() {
-        el.parentElement.parentElement.remove()
-        console.log("sas")
-    })
-}) 
+// Add a button on each book’s display to change its read status.
+// To facilitate this you will want to create the function that toggles a book’s read status on your Book prototype instance.
+
+document.querySelector(".main").addEventListener("click", function (e) {
+  if (e.target.classList.contains("book_status")) {
+    let bookCard = e.target.parentElement.parentElement;
+    // console.log(e.target.textContent)
+    if (e.target.textContent == "Not read") {
+      e.target.textContent = "Read";
+      myLibrary[
+        [...bookCard.parentElement.children].indexOf(bookCard)
+      ].readStatus = "yes";
+    } else if (e.target.textContent == "Read") {
+      e.target.textContent = "Not read";
+      myLibrary[
+        [...bookCard.parentElement.children].indexOf(bookCard)
+      ].readStatus = "no";
+    }
+  }
+});
